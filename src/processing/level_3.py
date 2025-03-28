65d2ccc69ab3e5ed364fcdec97cdf22b45ff1493
Lahdhiri Moetez <moetez.lahdhiri@student-cs.fr>
2025-03-29 00:34:09 +0100

    add rolling features on target

--- src/processing/level_3.py
+++ src/processing/level_3.py
@@ -13,11 +13,11 @@ class Level3Preprocessing():
         data_copy = data.copy()
         data_copy = data_copy.sort_values(by=[DatasetSchema.CUSTOMER_ID, DatasetSchema.YEAR_MONTH])
 
-        windows = list(range(2, 13))
+        windows = list(range(3, 13))
         methods = ['mean', 'median', 'std', 'sum']
         for window in windows:
             for method in methods:
-                feature_name = f"{DatasetSchema.ROLLING}_{method}_{window}m"
+                feature_name = f"{DatasetSchema.ROLLING}_{DatasetSchema.NB_TRANSACTIONS}_{method}_{window}m"
                 data_copy[feature_name] = (
                     data_copy.groupby(DatasetSchema.CUSTOMER_ID)[DatasetSchema.NB_TRANSACTIONS]
                     .rolling(window=window, min_periods=1 if method != 'std' else 2)
@@ -25,20 +25,40 @@ class Level3Preprocessing():
                     .reset_index(0, drop=True)
                 )
                 
-                if method == 'std':
-                    data_copy[feature_name] = data_copy[feature_name].fillna(0)
+                data_copy[feature_name] = data_copy[feature_name].fillna(0)
         
         return data_copy
 
-    def lag_features(self, data: pd.DataFrame) -> pd.DataFrame:
+    def rolling_features_target(self, data: pd.DataFrame) -> pd.DataFrame:
         data_copy = data.copy()
         data_copy = data_copy.sort_values(by=[DatasetSchema.CUSTOMER_ID, DatasetSchema.YEAR_MONTH])
 
-        lags = list(range(2, 13))
+        windows = list(range(3, 13))
+        methods = ['mean', 'median', 'std', 'sum']
+        for window in windows:
+            for method in methods:
+                feature_name = f"{DatasetSchema.ROLLING}_{DatasetSchema.FUTURE_TRANSACTIONS}_{method}_{window}m"
+                data_copy[feature_name] = (
+                    data_copy.groupby(DatasetSchema.CUSTOMER_ID)[DatasetSchema.FUTURE_TRANSACTIONS]
+                    .shift(3)
+                    .rolling(window=window, min_periods=1 if method != 'std' else 2)
+                    .agg(method)
+                    .reset_index(0, drop=True)
+                )
+                
+                data_copy[feature_name] = data_copy[feature_name].fillna(0)
+        
+        return data_copy
+    
+    def lag_features(self, data: pd.DataFrame, column: str) -> pd.DataFrame:
+        data_copy = data.copy()
+        data_copy = data_copy.sort_values(by=[DatasetSchema.CUSTOMER_ID, DatasetSchema.YEAR_MONTH])
+
+        lags = list(range(3, 13))
         for lag in lags:
-            lag_feature_name = f"{DatasetSchema.LAG}_{lag}"
+            lag_feature_name = f"{DatasetSchema.LAG}_{column}_{lag}m"
             data_copy[lag_feature_name] = (
-                data_copy.groupby(DatasetSchema.CUSTOMER_ID)[DatasetSchema.NB_TRANSACTIONS]
+                data_copy.groupby(DatasetSchema.CUSTOMER_ID)[column]
                 .shift(lag)
                 .fillna(0)
             )
@@ -49,5 +69,7 @@ class Level3Preprocessing():
         data_copy = data.copy()
         data_copy = self.tmp_features(data_copy)
         data_copy = self.rolling_features(data_copy)
-        data_copy = self.lag_features(data_copy)
+        data_copy = self.rolling_features_target(data_copy)
+        data_copy = self.lag_features(data_copy, column=DatasetSchema.NB_TRANSACTIONS)
+        data_copy = self.lag_features(data_copy, column=DatasetSchema.FUTURE_TRANSACTIONS)
         return data_copy
